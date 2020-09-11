7a97812852ecd1ee816911f71b9722c3d65491ee
Ben Ng <ben.ng@outlook.com>
2020-09-11 00:35:50 -0400

    added registration page stub

--- event-site/src/app/app-routing.module.ts
+++ event-site/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { RegisterPageComponent } from './login/register-page/register-page.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent }
 ];
 
 @NgModule({
--- event-site/src/app/login/login.module.ts
+++ event-site/src/app/login/login.module.ts
@@ -1,17 +1,20 @@
+import { RouterModule } from '@angular/router';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
 
 @NgModule({
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, RegisterPageComponent],
   imports: [
     CommonModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     LoginPageComponent
